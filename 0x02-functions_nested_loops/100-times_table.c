637220129a236979d0ea97f675b4f814f6bf4fe3
tolera <toleradegefa@outlook.com>
2022-03-15 22:19:56 +0300

    advanced 1st

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,48 +1,48 @@
-*
- * File: 100-times_table.c
- * Auth: Tolera Degefa
- */
-
-#include <stdio.h>
-
+#include "main.h"
 /**
- * print_times_table - Prints the times table of the input,
- *                     starting with 0.
- * @n: The value of the times table to be printed.
+ * print_times_table - prints the times table
+ * @n: takes in an integer
  */
 void print_times_table(int n)
 {
-int num, mult, prod;
+	int i, j, k;
 
-if (n >= 0 && n <= 15)
-{
-for (num = 0; num <= n; num++)
-{
-_putchar('0');
-
-for (mult = 1; mult <= n; mult++)
-{
-_putchar(',');
-_putchar(' ');
-
-prod = num * mult;
-
-if (prod <= 99)
-_putchar(' ');
-if (prod <= 9)
-_putchar(' ');
-if (prod >= 100)
-{
-_putchar((prod / 100) + '0');
-_putchar(((prod / 10)) % 10 + '0');
-}
-else if (prod <= 99 && prod >= 10)
-{
-_putchar((prod / 10) + '0');
-}
-_putchar((prod % 10) + '0');
-}
-_putchar('\n');
-}
-}
+	if (n > 15 || n < 0)
+		return;
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			k = i * j;
+			if (j == 0)
+			{
+				_putchar(k + '0');
+			}
+			else if (k <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			}
+			else if (k >= 10 && k < 100)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k / 10 + '0');
+				_putchar(k % 10 + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 100) + '0');
+				_putchar(k / 10 % 10 + '0');
+				_putchar(k % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
 }
