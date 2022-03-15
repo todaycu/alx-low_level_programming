52a206640659d7f07cb0784f24c6c969e5f64135
tolera <toleradegefa@outlook.com>
2022-03-15 18:47:43 +0300

    my second commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,13 @@
 #include "main.h"
-
 /**
- * main - check the code for Holberton School students.
+ * print_alphabet - prints alphabet in lowercase
  *
- * Return: Always 0.
- */
-int main(void)
+ **/
+void print_alphabet(void)
 {
-	print_alphabet();
-	return (0);
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
 }
