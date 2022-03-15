6f4b52c0d16de19aaa0cc974f2f2a8b46e650dba
Tolera Degefa <100357038+todaycu@users.noreply.github.com>
2022-03-15 23:22:18 +0300

    advanced 1st

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,48 +1,47 @@
 #include "main.h"
 /**
- * print_times_table - prints the times table
- * @n: takes in an integer
+ * print_times_table - Print the `n` times table, starting with 0.
+ * Description: If `n` is greater than 15 or less than 0, print nothing.
+ * @n: int type number
  */
 void print_times_table(int n)
 {
-	int i, j, k;
+	int x = 0, y, z;
 
 	if (n > 15 || n < 0)
 		return;
-	for (i = 0; i <= n; i++)
+	while (x <= n)
 	{
-		for (j = 0; j <= n; j++)
+		for (y = 0; y <= n; y++)
 		{
-			k = i * j;
-			if (j == 0)
+			z = x * y;
+			if (z > 99)
 			{
-				_putchar(k + '0');
+				_putchar(z / 100 + '0');
+				_putchar((z / 10 % 10) + '0');
+				_putchar(z % 10 + '0');
 			}
-			else if (k <= 9)
+			else if (z > 9)
 			{
-				_putchar(',');
 				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(k + '0');
+				_putchar(z / 10 + '0');
+				_putchar(z % 10 + '0');
 			}
-			else if (k >= 10 && k < 100)
+			else if (y != 0)
 			{
-				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
-				_putchar(k / 10 + '0');
-				_putchar(k % 10 + '0');
+				_putchar(z + '0');
 			}
 			else
+				_putchar(z + '0');
+			if (y != n)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar((k / 100) + '0');
-				_putchar(k / 10 % 10 + '0');
-				_putchar(k % 10 + '0');
 			}
 		}
 		_putchar('\n');
+		x++;
 	}
 }
