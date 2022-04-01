a2cebb2ceae7f885f0dcbfd0fcfd0bceb2c64c57
Tolera Degefa <100357038+todaycu@users.noreply.github.com>
2022-04-01 18:57:44 +0300

    Update 2-strncpy.c

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -1,25 +1,31 @@
 #include "main.h"
+
 /**
-*  _strncpy - C function that copies a string, including the
-*  terminating null byte, using at most an inputted number of bytes.
-*  If the length of the source string is less than the maximum byte number,
-*  the remainder of the destination string is filled with null bytes.
-*  Works identically to the standard library function `strncpy`.
-*@dest: buffer storing the string copy
-*@src:the source string
-*@n:max nummber of byte copied
-*Return: returns
-*/
+ * _strncpy - function that copies a string
+ *
+ * @dest: char pointer variable
+ * @src: char pointer variable
+ * @n: int type variable
+ *
+ * Return: a pointer to the resulting string dest
+ */
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+	int i, j;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		if (i < n)
+		{
+			dest[i] = src[i];
+		}
+	}
 
-	for ( ; i < n; i++)
-		dest[i] = '\0';
+	j = i;
+
+	for (; n > j; j++)
+		dest[j] = '\0';
 
 	return (dest);
 }
