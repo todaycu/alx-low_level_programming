9d6be32395c7f2e5f3429fdb126e7dadc9b36d5b
Tolera Degefa <100357038+todaycu@users.noreply.github.com>
2022-04-01 19:02:24 +0300

    Update 4-isalpha.c

--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -1,11 +1,20 @@
 #include "main.h"
 
 /**
- * _isalpha - checkes wheater its uppercase or not
- * @c: accepts chars or int to eval
- * Return: 1 or 0
+ * _isalpha - tests whether a character is from alphabet.
+ * @c: character to be checked.
+ *
+ * Return: 1 or 0 in otherwise.
  */
+
 int _isalpha(int c)
 {
-return (((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) ? 1 : 0);
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
