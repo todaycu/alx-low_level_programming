9c990973e56eda8c8b7ba54b8bddb9a569d63dd7
Tolera Degefa <100357038+todaycu@users.noreply.github.com>
2022-04-07 14:05:28 +0300

    program that multiplies two positive numbers.

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,59 +1,69 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 /**
-* _isnumber - check the code for Holberton School students.
-*
-* Return: 0 and 1.
-* @c: is a int assi.
-*/
-int _isnumber(char *c)
+  * _isdigit - tells if the string consists of digits
+  * @argv: pointer to current item in argument
+  * Return: return 0 if all digits, 1 if not all digits.
+  */
+int _isdigit(char *argv)
 {
 	int i;
 
-	for (i = 0; *c != '\0'; i++)
+	i = 0;
+	while (argv[i])
 	{
-		if (*c >= 48 && *c <= 57)
-		{
-			c++;
-		}
+		if (argv[i] >= '0' && argv[i] <= '9')
+			i++;
 		else
-		{
 			return (1);
-		}
 	}
 	return (0);
 }
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always int.
- * @argc: is a variable int.
- * @argv: is a variable char pointer.
- *
- */
+  * _atoi - converts a string of ascii digits to the values they represent
+  * @s: pointer to the source string
+  * Return: value of digits
+  */
+int _atoi(char *s)
+{
+	int i, result;
+
+	i = result = 0;
+	while (s[i])
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result *= 10;
+			result += (s[i] - '0');
+		}
+		i++;
+	}
+	return (result);
+}
+/**
+  * main - main function call
+  * @argc: argument count
+  * @argv: 2D array of arguments
+  * Return: return 0 on success, 98 on failure
+  */
 int main(int argc, char *argv[])
 {
-	int mul1, mul2, i, result;
+	int i;
 
+	malloc();
 	if (argc != 3)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
-	mul1 = atoi(argv[1]);
-	mul2 = atoi(argv[2]);
-
 	for (i = 1; i < argc; i++)
 	{
-		if (_isnumber(argv[i]) == 1)
+		if (_isdigit(argv[i]))
 		{
 			printf("Error\n");
 			exit(98);
 		}
 	}
-	result = mul1 * mul2;
-	printf("%i\n", result);
 	return (0);
 }
