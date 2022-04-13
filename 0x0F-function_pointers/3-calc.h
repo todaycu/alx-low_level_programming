a140f8e55c8c012346637ba5715d1133756662c8
Tolera Degefa <100357038+todaycu@users.noreply.github.com>
2022-04-13 20:15:00 +0300

    performs simple operation

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,15 +1,16 @@
 #ifndef CALC_H
 #define CALC_H
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
-* struct op - Struct op
-*
-* @op: The operator
-* @f: The function associated
-*/
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
 typedef struct op
 {
 	char *op;
@@ -23,4 +24,4 @@ int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
 
-#endif /* CALC_H */
+#endif
