57950d3d59f9159806864dd2def94fe79daa1d75
Tolera Degefa <100357038+todaycu@users.noreply.github.com>
2022-04-13 20:24:03 +0300

    simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,16 +1,17 @@
 #include "3-calc.h"
 
 /**
-* main - calls other functions
-* @argc: argument count
-* @argv: argument vector
-*
-* Return: error if conditions not met
-*/
+ * main - program that perfroms simple operations
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
-	int i = 0, j = 0, ret = 0;
-	char s;
+	int arg1, arg2, result;
+	char o;
+	int (*func)(int, int);
 
 	if (argc != 4)
 	{
@@ -18,22 +19,28 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	if (argv[2][1] != '\0')
+	arg1 = atoi(argv[1]);
+	arg2 = atoi(argv[3]);
+
+	func = get_op_func(argv[2]);
+
+	if (!func)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	s = argv[2][0];
-	if (s != '+' && s != '-' && s != '/' && s != '*' && s != '%')
+	o = *argv[2];
+
+	if ((o == '/' || o == '%') && arg2 == 0)
 	{
 		printf("Error\n");
-		exit(99);
+		exit(100);
 	}
 
-	i = atoi(argv[1]);
-	j = atoi(argv[3]);
-	ret = (get_op_func(argv[2]))(i, j);
-	printf("%d\n", ret);
+	result = func(arg1, arg2);
+
+	printf("%d\n", result);
+
 	return (0);
 }
