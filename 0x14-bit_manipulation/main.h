2038873cabd761cf1acf2f7e5fe82a8263d9af47
Tolera Degefa <100357038+todaycu@users.noreply.github.com>
2022-04-28 12:36:21 +0300

    Update main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,8 +5,6 @@
  * Desc: Header file containing prototypes for all functions
  *       written in the 0x13-bit_manipulation directory.
  */
-
-int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
